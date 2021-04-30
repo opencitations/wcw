ddba9eb5bce94dc46d8acba26bf3461d6badd808
Simone Persiani <iosonopersia@gmail.com>
2021-04-30 12:28:25 +0200

    Upload test module

--- Pusher/test/test_orcid_utils.py
+++ Pusher/test/test_orcid_utils.py
@@ -14,18 +14,93 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 import unittest
+from id_utils.orcid_utils import normalize_orcid, orcid_is_valid, orcid_check_digit
 
 
 class TestOrcidUtils(unittest.TestCase):
 
     def test_normalize_orcid(self):
-        pass
+        string = '0s 0\n00-23!amnsfb45-4567-___5678'
+        result = normalize_orcid(string)
+        self.assertIsNotNone(result)
+
+        self.assertEqual(result, '0000-2345-4567-5678')
 
     def test_orcid_is_valid(self):
-        pass
+        with self.subTest('Missing first dash'):
+            string = '00001234-5678-9876'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Missing second dash'):
+            string = '0000-12345678-9876'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Missing third dash'):
+            string = '0000-1234-56789876'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('ORCID too short'):
+            string = '0000-0000'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('ORCID too long'):
+            string = '0000-0000-0000-0000-0000'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('X not in the last position'):
+            string = '0000-00X0-0000-0000'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Dash in the wrong position'):
+            string = '00-00000-0-00000000'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Wrong check digit'):
+            string = '0000-0002-1825-0093'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Valid ORCID'):
+            string = '0000-0002-1825-0097'
+            result = orcid_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertTrue(result)
 
     def test_orcid_check_digit(self):
-        pass
+        with self.subTest('ORCID 1'):
+            string = '0000-0002-1825-0097'
+            result = orcid_check_digit(string)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, '7')
+        with self.subTest('ORCID 2'):
+            string = '0000-0001-2345-6789'
+            result = orcid_check_digit(string)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, '9')
+        with self.subTest('ORCID 3'):
+            string = '0000-0002-1694-233X'
+            result = orcid_check_digit(string)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, 'X')
 
 
 if __name__ == '__main__':
