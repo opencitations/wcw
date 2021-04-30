d2bdeb06a6290493573047575b073fa30e10b889
Simone Persiani <iosonopersia@gmail.com>
2021-04-30 12:35:48 +0200

    Upload test module

--- Pusher/test/test_issn_utils.py
+++ Pusher/test/test_issn_utils.py
@@ -14,18 +14,81 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 import unittest
+from id_utils.issn_utils import normalize_issn, issn_is_valid, issn_check_digit
 
 
 class TestIssnUtils(unittest.TestCase):
 
     def test_normalize_issn(self):
-        pass
+        string = '0s 0\n00-23!amnsfb45'
+        result = normalize_issn(string)
+        self.assertIsNotNone(result)
+
+        self.assertEqual(result, '0000-2345')
 
     def test_issn_is_valid(self):
-        pass
+        with self.subTest('Missing dash'):
+            string = '00001234'
+            result = issn_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('ISSN too short'):
+            string = '000-000'
+            result = issn_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('ISSN too long'):
+            string = '0000-0000-0000-0000'
+            result = issn_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('X not in the last position'):
+            string = '0000-00X0'
+            result = issn_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Dash in the wrong position'):
+            string = '00-00000'
+            result = issn_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Wrong check digit'):
+            string = '2049-363X'
+            result = issn_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertFalse(result)
+        with self.subTest('Valid ISSN'):
+            string = '2049-3630'
+            result = issn_is_valid(string)
+            self.assertIsNotNone(result)
+
+            self.assertTrue(result)
 
     def test_issn_check_digit(self):
-        pass
+        with self.subTest('ISSN 1'):
+            string = '2049-3630'
+            result = issn_check_digit(string)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, '0')
+        with self.subTest('ISSN 2'):
+            string = '0028-0836'
+            result = issn_check_digit(string)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, '6')
+        with self.subTest('ISSN 3'):
+            string = '0954-349X'
+            result = issn_check_digit(string)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, 'X')
 
 
 if __name__ == '__main__':
