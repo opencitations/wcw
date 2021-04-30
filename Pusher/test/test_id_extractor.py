4e6658e1009e0bd4fbd48be2fbbf537ade2591c0
Simone Persiani <iosonopersia@gmail.com>
2021-04-30 20:46:25 +0200

    Upload test module

--- Pusher/test/test_id_extractor.py
+++ Pusher/test/test_id_extractor.py
@@ -14,12 +14,37 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 import unittest
+from oc_ocdm.graph import GraphSet
+from id_extractor import extract_ids
 
 
 class TestIdExtractor(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_extract_ids(self):
+        resp_agent = 'http://w3c.org/oc/meta/pa/999'
+        g_set = GraphSet('http://w3c.org/oc/meta/')
+        br = g_set.add_br(resp_agent)
+
+        isbn = g_set.add_id(resp_agent)
+        isbn.create_isbn('978-88-515-2159-2')
+
+        orcid = g_set.add_id(resp_agent)
+        orcid.create_orcid('0000-0002-1825-0097')
+
+        wikidata = g_set.add_id(resp_agent)
+        wikidata.create_wikidata('Q9')
+
+        br.has_identifier(isbn)
+        br.has_identifier(orcid)
+        br.has_identifier(wikidata)
+
+        result = extract_ids(br)
+        self.assertIsNotNone(result)
+
+        self.assertDictEqual(result, {'isbn13': '978-88-515-2159-2',
+                                      'isbn10': '88-515-2159-X',  # this is automatically inferred
+                                      'orcid': '0000-0002-1825-0097',
+                                      'wikidata': 'Q9'})
 
 
 if __name__ == '__main__':
