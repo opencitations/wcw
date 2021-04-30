c618eb8a54b68d869d9eb83df731fbef1a278405
Simone Persiani <iosonopersia@gmail.com>
2021-04-30 22:32:41 +0200

    Upload test modules

--- Converter/test/test_authors_utils.py
+++ Converter/test/test_authors_utils.py
@@ -14,12 +14,82 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 import unittest
+from utils.authors_utils import extract_author_name, invalid_brackets, unify_duplicated_equal_signs,\
+                                clean_author_string, get_author_dict, parse_authors
 
 
 class TestAuthorsUtils(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_extract_author_name(self):
+        with self.subTest('only first'):
+            result = extract_author_name({'first': 'Jo,hn'})
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, ', John')
+        with self.subTest('only last'):
+            result = extract_author_name({'last': 'P.'})
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, 'P., ')
+        with self.subTest('first and last'):
+            result = extract_author_name({'first': 'Jo,hn', 'last': 'P.'})
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, 'P., John')
+        with self.subTest('link with comma'):
+            result = extract_author_name({'link': 'John, P.'})
+            self.assertIsNotNone(result)
+
+            self.assertEqual(result, 'John, P.')
+        with self.subTest('link without comma'):
+            result = extract_author_name({'link': 'John P.'})
+            self.assertIsNone(result)
+
+    def test_invalid_brackets(self):
+        self.assertTrue(invalid_brackets(-1, 8))
+        self.assertTrue(invalid_brackets(8, -1))
+        self.assertTrue(invalid_brackets(8, 8))
+        self.assertTrue(invalid_brackets(8, 6))
+        self.assertFalse(invalid_brackets(5, 10))
+
+    def test_unify_duplicated_equal_signs(self):
+        result = unify_duplicated_equal_signs('abc===def, mno= =pqr, stv==q==  =')
+        self.assertIsNotNone(result)
+
+        self.assertEqual(result, 'abc=def, mno=pqr, stv=q=')
+
+    def test_clean_author_string(self):
+        result = clean_author_string('  abc===[def, \u0001\n\rmno= ]=pqr, stv==q==]  =   ')
+        self.assertIsNotNone(result)
+
+        self.assertEqual(result, 'abc=def, mno=pqr, stv=q=')
+
+    def test_get_author_dict(self):
+        result = get_author_dict('first=Clark, last===Kent, link==Superman')
+        self.assertIsNotNone(result)
+
+        self.assertDictEqual(result, {'first': 'Clark', 'last': 'Kent', 'link': 'Superman'})
+
+    def test_parse_authors(self):
+        with self.subTest('Valid string'):
+            result = parse_authors('[{first=Clark, last===Kent, link==Superman}, {first=Albert, last=Einstein}]')
+            self.assertIsNotNone(result)
+
+            self.assertListEqual(result, [{'first': 'Clark', 'last': 'Kent', 'link': 'Superman'},
+                                          {'first': 'Albert', 'last': 'Einstein'}])
+        with self.subTest('Invalid strings (ignore_errors=False)'):
+            self.assertRaises(ValueError, parse_authors, '{first=Clark,] [last===Kent, link==Superman},'
+                                                         ' {first=Albert, last=Einstein}]', False)
+            self.assertRaises(ValueError, parse_authors, '[{}, {first=Clark, last=,==Kent, link==Superman},'
+                                                         ' {first=Albert, last=Einstein}]', False)
+            self.assertRaises(ValueError, parse_authors, '[]', False)
+
+        with self.subTest('Invalid strings (ignore_errors=True)'):
+            self.assertListEqual(parse_authors('{first=Clark, [last===Kent, link==Superman},'
+                                               ' {first=Albert, last=Einstein}]', True), [])
+            self.assertListEqual(parse_authors('[{}, {first=Clark, last=,==Kent, link==Superman},'
+                                               ' {first=Albert, last=Einstein}]', True), [])
+            self.assertListEqual(parse_authors('[]', True), [])
 
 
 if __name__ == '__main__':
--- Converter/test/test_id_list_utils.py
+++ Converter/test/test_id_list_utils.py
@@ -14,12 +14,55 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 import unittest
-
+from utils.id_list_utils import invalid_brackets, clean_id_list_string,\
+                                parse_id_list, parse_id_list_repeated_schemes
 
 class TestIdUtils(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_invalid_brackets(self):
+        self.assertTrue(invalid_brackets(-1, 8))
+        self.assertTrue(invalid_brackets(8, -1))
+        self.assertTrue(invalid_brackets(8, 8))
+        self.assertTrue(invalid_brackets(8, 6))
+        self.assertFalse(invalid_brackets(5, 10))
+
+    def test_clean_id_list_string(self):
+        result = clean_id_list_string('  isbn=def, \u0001\n\rissn=pqr  ')
+        self.assertIsNotNone(result)
+
+        self.assertEqual(result, 'isbn=def, issn=pqr')
+
+    def test_parse_id_list(self):
+        with self.subTest('Valid string'):
+            result = parse_id_list('{doi=xyz, isbn=abc, doi=PQR}')
+            self.assertIsNotNone(result)
+
+            self.assertDictEqual(result, {'doi': 'PQR', 'isbn': 'abc'})
+
+        with self.subTest('Invalid strings (ignore_errors=False)'):
+            self.assertRaises(ValueError, parse_id_list, 'doi=xyz,} isbn=abc, doi=PQR{', False)
+            self.assertRaises(ValueError, parse_id_list, '{doi=xy=z, isbn=abc, doi=PQR}', False)
+            self.assertRaises(ValueError, parse_id_list, '{asgr=xxx, poiaf=yyy}', False)
+
+        with self.subTest('Invalid strings (ignore_errors=True)'):
+            self.assertDictEqual(parse_id_list('doi=xyz,{ isbn=abc, doi=PQR{', True), {})
+            self.assertDictEqual(parse_id_list('{doi=xy=z, isbn=abc, doi=PQR}', True), {})
+            self.assertDictEqual(parse_id_list('{asgr=xxx, poiaf=yyy}', True), {})
+
+    def test_parse_id_list_repeated_schemes(self):
+        with self.subTest('Valid string'):
+            result = parse_id_list_repeated_schemes('tmp:xyz tmp:abc meta:PQR')
+            self.assertIsNotNone(result)
+
+            self.assertDictEqual(result, {'tmp': ['xyz', 'abc'], 'meta': ['PQR']})
+
+        with self.subTest('Invalid strings (ignore_errors=False)'):
+            self.assertRaises(ValueError, parse_id_list_repeated_schemes, 'tmp:xyz tmp:abc metaPQR', False)
+            self.assertRaises(ValueError, parse_id_list_repeated_schemes, 'doi:xyz issn:abc doi:PQR', False)
+
+        with self.subTest('Invalid strings (ignore_errors=True)'):
+            self.assertDictEqual(parse_id_list_repeated_schemes('tmp:xyz tmp:abc metaPQR', True), {})
+            self.assertDictEqual(parse_id_list_repeated_schemes('doi:xyz issn:abc doi:PQR', True), {})
 
 
 if __name__ == '__main__':
